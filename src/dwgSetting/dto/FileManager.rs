e77e0f3081c445b83035e60a1c66d13a45fad4ed
kjh0727 <kjh0727@midasit.com>
2024-05-02 11:36:18 +0900

    <추가>(Savefile) : savefile 추가 및 라우터 수정

---
+++ src/dwgSetting/dto/FileManager.rs
@@ -0,0 +1,12 @@
+pub async fn SaveFile(dwg: String)-> std::io::Result<()> 
+{
+    let from_path_str = format!(r"C:\MIDAS\{}", dwg);
+    let from = std::path::Path::new(&from_path_str);
+    
+    let to_path_str = format!(r"D:\rust_study\RustStudy\Temp_Cloud\{}", dwg);
+    let to = std::path::Path::new(&to_path_str);
+    
+    std::fs::copy(&from, &to)?;
+
+    Ok(())
+}
